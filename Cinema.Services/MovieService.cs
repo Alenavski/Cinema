9cb7c86556ae0b239786e342d671330e45bb6a77
Alenavski <boom.alen.qwerty@gmail.com>
2021-10-13 13:38:17 +0300

    crud methods for movies

--- Cinema.Services/MovieService.cs
+++ Cinema.Services/MovieService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Cinema.DB.EF;
 using Cinema.DB.Entities;
 using Cinema.Services.Dtos;
@@ -12,26 +13,78 @@ namespace Cinema.Services
 {
     public class MovieService : IMovieService
     {
-        private ApplicationContext _context;
+        private readonly ApplicationContext _context;
 
         public MovieService(ApplicationContext context)
         {
             _context = context;
         }
 
+        public async Task<int> AddMovieAsync(MovieDto movie)
+        {
+            var movieEntity = movie.Adapt<MovieEntity>();
+            await _context.Movies.AddAsync(movieEntity);
+            await _context.SaveChangesAsync();
+            return movieEntity.Id;
+        }
+
+        public async Task<MovieDto> GetMovieByIdAsync(int id)
+        {
+            return await _context.Movies
+                .Where(m => m.Id == id)
+                .ProjectToType<MovieDto>()
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task DeleteMovieAsync(int id)
+        {
+            var movie = await _context.Movies.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateMovieAsync(MovieDto movieDto)
+        {
+            var movie = await _context.Movies.SingleOrDefaultAsync(m => m.Id == movieDto.Id);
+
+            movie.Description = movieDto.Description;
+            movie.Poster = movieDto.Poster ?? movie.Poster;
+            movie.Title = movieDto.Title;
+            movie.EndDate = movieDto.EndDate;
+            movie.StartDate = movieDto.StartDate;
+
+            await _context.SaveChangesAsync();
+        }
+
         public IEnumerable<MovieDto> GetMoviesByFilter(ShowtimeFilterDto filter)
         {
+            filter.StartTime ??= new TimeSpan(0, 0, 0);
             filter.EndTime ??= new TimeSpan(23, 59, 59);
             var showtimes = _context.Showtimes
                 .Include(s =>s.Hall)
                 .ThenInclude(h => h.Cinema)
                 .Include(s => s.Movie)
-                .Where(s => s.Hall.Cinema.City == filter.City)
-                .Where(s =>
-                    DateTime.Compare(s.Movie.StartDate, filter.Date) <= 0
-                    && DateTime.Compare(s.Movie.EndDate, filter.Date) >= 0)
-                .Where(s => s.Time >= filter.StartTime && s.Time <= filter.EndTime)
-                .Where(s => s.NumberOfFreeSeats >= filter.NumberOfFreeSeats);
+                .Where(s => s.Time >= filter.StartTime && s.Time <= filter.EndTime);
+
+            if (filter.City != null)
+            {
+                showtimes = showtimes
+                    .Where(s => s.Hall.Cinema.City == filter.City);
+            }
+
+            if (!filter.Date.Equals(DateTime.MinValue))
+            {
+                showtimes = showtimes
+                    .Where(s =>
+                        DateTime.Compare(s.Movie.StartDate, filter.Date) <= 0
+                        && DateTime.Compare(s.Movie.EndDate, filter.Date) >= 0);
+            }
+
+            if (filter.NumberOfFreeSeats != 0)
+            {
+                showtimes = showtimes
+                    .Where(s => s.NumberOfFreeSeats >= filter.NumberOfFreeSeats);
+            }
 
             if (filter.MovieTitle != null)
             {
