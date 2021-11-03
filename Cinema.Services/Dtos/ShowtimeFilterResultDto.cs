d5ec4360bbc85af1495147a33379578a563657c1
Alenavski <boom.alen.qwerty@gmail.com>
2021-11-03 16:04:24 +0300

    remove unused fields

--- Cinema.Services/Dtos/ShowtimeFilterResultDto.cs
+++ Cinema.Services/Dtos/ShowtimeFilterResultDto.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Cinema.Services.Dtos
 {
@@ -7,29 +6,17 @@ namespace Cinema.Services.Dtos
     {
         public long Id { get; set; }
         public TimeSpan Time { get; set; }
-        public short NumberOfFreeSeats { get; set; }
-        public HallDto Hall { get; set; }
         public MovieDto Movie { get; set; }
-        public ICollection<AdditionDto> Additions { get; set; }
-        public ICollection<TicketPriceDto> Prices { get; set; }
 
         public ShowtimeFilterResultDto(
             long id,
             TimeSpan time,
-            short numberOfFreeSeats,
-            HallDto hall,
-            MovieDto movie,
-            ICollection<AdditionDto> additions,
-            ICollection<TicketPriceDto> prices
+            MovieDto movie
         )
         {
             Id = id;
             Time = time;
-            NumberOfFreeSeats = numberOfFreeSeats;
-            Hall = hall;
             Movie = movie;
-            Additions = additions;
-            Prices = prices;
         }
     }
 }
