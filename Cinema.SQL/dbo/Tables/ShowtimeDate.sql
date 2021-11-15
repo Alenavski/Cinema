1a3d3de23c2184ebf75230e193b8b3238b620a1d
Alenavski <boom.alen.qwerty@gmail.com>
2021-11-15 14:57:08 +0300

    wip

---
+++ Cinema.SQL/dbo/Tables/ShowtimeDate.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ShowtimeDate] (
+    [id]         BIGINT        NOT NULL,
+    [showtimeId] BIGINT        NOT NULL,
+    [Date]       DATETIME2 (7) NOT NULL,
+    CONSTRAINT [PK_ShowtimeDate] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_ShowtimeDate_ShowtimeDate] FOREIGN KEY ([showtimeId]) REFERENCES [dbo].[Showtimes] ([Id])
+);
+
